2e6d08f8f0163976e5f9f179ceb598a543cb65d0
Evgen <e.salyakin@gmail.com>
2017-08-16 22:09:37 +0300

    Evgen: deploy Heroku

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var mongoose = require('mongoose');
 var uristring =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
+    process.env.MONGODB_URI ||
     'mongodb://localhost/urandb';
 
 var options = { promiseLibrary: require('bluebird') };
